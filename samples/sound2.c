4a54eb7322913ffaf03b3caaa7eb0e0a5157c2be
soyoil <kappamaki8801@gmail.com>
2019-12-15 12:41:50 +0900

    f

--- samples/sound2.c
+++ samples/sound2.c
@@ -4,18 +4,18 @@ void main() {
   int freq[] = {0x60AU, 0x642U, 0x672U, 0x689U, 0x6B2U, 0x6D6U, 0x6F7U, 0x706U};
   int i;
 
-  NR52_REG = 0x80U;
-  NR50_REG = 0x77U;
-  NR51_REG = 0xFFU;
+  NR52_REG = 0x80U;                           // サウンド機能ON
+  NR50_REG = 0x77U;                           // 左右ともに最大出力に設定
+  NR51_REG = 0xFFU;                           // ch1の出力ON
 
-  NR10_REG = 0x00U;
-  NR11_REG = 0x83U;
-  NR12_REG = 0xF0U;
+  NR10_REG = 0x00U;                           // スイープ設定なし
+  NR11_REG = 0x83U;                           // デューティー比50%, 長さ設定なし
+  NR12_REG = 0xF0U;                           // 音量最大, エンベロープ設定なし
   
   for(i = 0; i < 8; i++){
-    NR13_REG = freq[i] % 0x100U;
-    NR14_REG = 0x80U + (freq[i] / 0x100U);
-    delay(1000);
+    NR13_REG = freq[i] % 0x100U;              // 周波数の下位8ビットを代入
+    NR14_REG = 0x80U + (freq[i] / 0x100U);    // 周波数の上位4ビットとフラグ類を代入
+    delay(1000);                              // 1秒待つ
   }
 
   NR52_REG = 0x00U;
