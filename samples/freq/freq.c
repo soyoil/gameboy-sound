378394caa941cb21ae2b036694d325e5e6936026
soyoil <kappamaki8801@gmail.com>
2019-12-19 22:46:09 +0900

    modify

--- samples/freq/freq.c
+++ samples/freq/freq.c
@@ -6,7 +6,7 @@ void main() {
 
   NR52_REG = 0x80U;                           // サウンド機能ON
   NR50_REG = 0x77U;                           // 左右ともに最大出力に設定
-  NR51_REG = 0xFFU;                           // ch1の出力ON
+  NR51_REG = 0x11U;                           // ch1の出力ON
 
   NR10_REG = 0x00U;                           // スイープ設定なし
   NR11_REG = 0x83U;                           // デューティー比50%, 長さ設定なし
