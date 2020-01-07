9941ba4607857c29085f44e20b308ce0e99c5388
soyoil <kappamaki8801@gmail.com>
2020-01-07 23:57:57 +0900

    ページめくりとhelpを追加

--- samples/regedit/regedit.c
+++ samples/regedit/regedit.c
@@ -11,67 +11,115 @@ char *regname[23] = {
     "NR40", "NR41", "NR42", "NR43", "NR44",
     "NR50", "NR51", "NR52"
 };
+int nowPage = 0;
 
-void init(){
+void display(int channel){
     int i, j, digit, base;
     char bin[12] = "00000000";
-    NR52_REG = 0x80U;
-    for(i = 0xff10; i != 0xff26; i++) (*(__REG)i) = 0x00U;
-    for(i = 0; i < 23; i++) regs[i] = 0;
-    regs[23] = 0x80U;
-
     gotogxy(0, 0);
     gprintf("----SOUND EDITOR----");
     for(i = 0; i < 5; i++){
-        gotogxy(2, 3 * i + 2);
+        gotogxy(1, 3 * i + 2);
         strcpy(bin, "00000000");
         j = 7;
         base = 1;
-        digit = regs[i];
+        digit = regs[i + 5 * channel];
         while (digit > 0){
             if(digit % 2) bin[j] = '1';
             digit /= 2;
             j--;
         }
-        if(regs[i] < 16){
-            gprintf("%s 0%x", regname[i], regs[i]);
+        if(channel == 4 && i > 2){
+            gprintf("                   ");
+            gotogxy(0, 3 * i + 3);
+            gprintf("                   ");
+            continue;
+        }
+        else if(regs[i] < 16){
+            gprintf(" %s 0%x ", regname[i + 5 * channel], regs[i + 5 * channel]);
         }else{
-            gprintf("%s %x", regname[i], regs[i]);
+            gprintf(" %s %x ", regname[i + 5 * channel], regs[i + 5 * channel]);
         }
         gotogxy(10, 3 * i + 2);
         gprintf("%s", bin);
+        gotogxy(0, 3 * i + 3);
+        gprintf("                   ");
     }
     gotogxy(1, 2);
     wrtchr(259);
     gotogxy(0, 17);
-    gprintf("-SELECT: help---1/4-");
+    gprintf("-SELECT: help---%d/5-", nowPage+1);
+}
+
+void init(){
+    int i;
+    NR52_REG = 0x80U;
+    for(i = 0xff10; i != 0xff26; i++) (*(__REG)i) = 0x00U;
+    for(i = 0; i < 23; i++) regs[i] = 0;
+    regs[23] = 0x80U;
+    display(0);
 }
 
 void play(){
-
+    gotogxy(0, 17);
+    gprintf("-PLAYING--------1/5-");
 }
 
 void scroll(int direction){
-
+    direction++;
 }
 
 void edit(int cursor){
+    cursor++;
     // upperArrow 257
 }
 
 void changePage(int direction){
-
+    if(direction && nowPage != 0){
+        nowPage--;
+        display(nowPage);
+    }else if(!direction && nowPage != 4){
+        nowPage++;
+        display(nowPage);
+    }
 }
 
 void help(){
-
+    int i;
+    char *keys[5] = {
+        "Up,Down key:       ",
+        "Left,Right key:    ",
+        "A button:          ",
+        "B button:          ",
+        "START button:      "
+        };
+    char *insts[5] = {
+        "select register",
+        "change channels",
+        "edit value",
+        "Back",
+        "play/stop"
+    };
+    gotogxy(0, 0);
+    gprintf("-----HOW TO USE-----");
+    for(i = 0; i < 5; i++){
+        gotogxy(1, 3 * i + 2);
+        gprintf(keys[i]);
+        gotogxy(3, 3 * i + 3);
+        gprintf(insts[i]);
+    }
+    gotogxy(9, 17);
+    gprintf("back------");
+    delay(100);
+    while(!(joypad() & J_SELECT)) delay(1);
+    display(nowPage);
 }
 
 void main(){
     int isPlay = 0;
     int keyState = 0;
     int cursor = 0;
-    int i;
+    //int i;
 
     init();
 
@@ -89,6 +137,9 @@ void main(){
             changePage(1);
         }else if(keyState & J_START){
             play();
+        }else if(keyState & J_SELECT){
+            help();
         }
+        if(keyState) delay(100);
     }
 }
